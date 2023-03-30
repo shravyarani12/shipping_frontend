20e8ea242ab59d8edc47d9d660c49e2dd023231b
Shravya rani <shravyarani.gre@gmail.com>
2023-03-30 15:52:30 -0400

    replace txtinput

--- screens/TxtInput.js
+++ screens/TxtInput.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react'
-import { Text, StyleSheet, SafeAreaView, Keyboard, TouchableWithoutFeedback, View, Pressable, Image } from 'react-native';
-import { Stack, TextInput, IconButton } from "@react-native-material/core";
-import { LogBox  } from 'react-native';
+import React, { useEffect } from 'react'
+import { Text, StyleSheet, SafeAreaView, Keyboard, TouchableWithoutFeedback, View, Pressable, Image,TextInput } from 'react-native';
+import { Stack, IconButton } from "@react-native-material/core";
+import { LogBox } from 'react-native';
 
 import FontAwesome5 from 'react-native-vector-icons/MaterialIcons';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -9,17 +9,33 @@ export default function TxtInput(props) {
 
     useEffect(() => {
         LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
-      }, [])
+    }, [])
 
     return (
         <View style={styles.inputContainer}>
-            <TextInput
+            {/* <TextInput
                 variant='outlined'
                 value={props.state[props.stateKey]}
                 onChangeText={newText => props.handleChange(newText, props.stateKey)}
                 label={props.label}
 
+            /> */}
+
+            <TextInput
+                type="text"
+                style={{
+                    height: 40,
+                    margin: 12,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 12
+                }}
+                value={props.state[props.stateKey]}
+                onChangeText={newText => props.handleChange(newText, props.stateKey)}
+                placeholder={props.labe}
             />
+
+
             {props.state[props.stateKey].length > 0 &&
                 <FontAwesome5 style={styles.icon} name={"delete"} size={20} onPress={() =>
                     props.handleChange("", props.stateKey)} />
